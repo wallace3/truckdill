aa26652fedcb9c0f7412e1f734a7555bf1ffd564
jelias <jelias@quarksoft.net>
2020-06-13 13:54:05 -0500

    Estructura Template
    se realizo la estructura base  del template

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link href="template/img/logo/logo.png" rel="icon" />
+    <title>TruckDill </title>
+    <link
+      href="template/vendor/fontawesome-free/css/all.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="template/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link href="template/css/ruang-admin.min.css" rel="stylesheet" />
+  </head>
+
+  <body id="page-top">
+    <div id="wrapper">
+        <?php  include "menuBase.php"?>
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <?php include "topBarBase.php" ?>
+
+          <!--Contenido-->
+          
+          <!---Fin de Contenido-->
+        </div>
+        <?php include "footerBase.php"?>
+      </div>
+    </div>
+
+    <!-- Scroll to top -->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <script src="template/vendor/jquery/jquery.min.js"></script>
+    <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="template/js/ruang-admin.min.js"></script>
+    <script src="template/vendor/chart.js/Chart.min.js"></script>
+    <script src="template/js/demo/chart-area-demo.js"></script>
+  </body>
+</html>
---
+++ menuBase.php
@@ -0,0 +1,31 @@
+  <!-- Sidebar -->
+  <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+          <img src="">
+        </div>
+        <div class="sidebar-brand-text mx-3">TruckDill</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Menú
+      </div>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <i class="fab fa-fw fa-wpforms"></i>
+          <span>Página 1</span>
+        </a>
+      </li>
+    
+
+      <hr class="sidebar-divider">
+      <div class="version" id="version-ruangadmin"></div>
+    </ul>
+    <!-- Sidebar -->

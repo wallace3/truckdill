2607d4c0addd7755490bfeb19b85b0ec5a62523a
Edgar Hernandez <edhernandezw@gmail.com>
2020-06-24 00:16:47 -0500

    alta facturas

---
+++ alta_facturas.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <link href="template/img/logo/logo.png" rel="icon" />
+        <title>TruckDill </title>
+        <link
+            href="template/vendor/fontawesome-free/css/all.min.css"
+            rel="stylesheet"
+            type="text/css"
+        />
+        <link
+            href="template/vendor/bootstrap/css/bootstrap.min.css"
+            rel="stylesheet"
+            type="text/css"
+        />
+        <link href="template/css/ruang-admin.min.css" rel="stylesheet" />
+        <link href="template/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    </head>
+
+    <body id="page-top">
+        <div id="wrapper">
+            <?php  include "menuBase.php"?>
+            <div id="content-wrapper"  class="d-flex flex-column">
+                <div id="content">
+                    <?php include "topBarBase.php" ?>
+                    <div id = "content-wrapper" class ="container-fluid">
+
+                        <!--Contenido-->
+                        
+                        <div class="card mb-4">
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                <h6 class="m-0 font-weight-bold text-primary">Alta de Usuarios</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile">
+                                        <label class="custom-file-label" for="customFile">Choose file</label>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail3" class="col-sm-3 col-form-label">Email</label>
+                                    <div class="col-sm-9">
+                                        <input type="email" class="form-control" id="correo" placeholder="Email" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail3" class="col-sm-3 col-form-label">Usuario</label>
+                                    <div class="col-sm-9">
+                                        <input type="email" class="form-control" id="usuario" placeholder="Usuario" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail3" class="col-sm-3 col-form-label">Tipo de usuario</label>
+                                    <div class = "col-sm-9">
+                                        <select class="form-control mb-3 types" id="types" required>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputPassword3" class="col-sm-3 col-form-label">Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="password" class="form-control" id="password" placeholder="Password" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-10">
+                                        <button type="button" onclick="createUser();" class="btn btn-primary">Crear</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!---Fin de Contenido-->
+                </div>
+                <?php include "footerBase.php"?>
+            </div>
+        </div>
+
+        <!--Modals-->
+
+        <div class="modal" id="modalSuccess" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Éxito</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Registro guardado exitosamente</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">OK</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal" id="modalError" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Éxito</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Ocurrio un error al crear usuario</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Fin Modals-->
+
+        <!-- Scroll to top -->
+        <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+        </a>
+
+        <script src="template/vendor/jquery/jquery.min.js"></script>
+        <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="template/js/ruang-admin.min.js"></script>
+        <script src="js/facturas.js"></script>
+
+     
+    </body>
+</html>
